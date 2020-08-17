4b8ab4ac4be397411bdd2ee3b92430a1f74a20f8
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-08-17 13:08:52 +0200

    Add needed classes to implement bean validation

---
+++ src/main/java/de/openknowledge/fruehauf/domain/TodoValidationErrorPayload.java
@@ -0,0 +1,42 @@
+package de.openknowledge.fruehauf.domain;
+
+import de.openknowledge.fruehauf.infrastructure.rest.error.ValidationErrorPayload;
+
+public class TodoValidationErrorPayload {
+
+    public static class NegativeTodoId extends ValidationErrorPayload {
+        public NegativeTodoId() {
+            super("NEGATIVE_TODO_ID", "todoId must be greater than or equal to 0");
+        }
+    }
+
+    public static class TitleIsInvalid extends ValidationErrorPayload {
+        public TitleIsInvalid() {
+            super("TITLE_NULL", "title must not be null");
+        }
+    }
+
+    public static class TitleSize extends ValidationErrorPayload {
+        public TitleSize() {
+            super("TITLE_SIZE", "title size must be between 1 and 30");
+        }
+    }
+
+    public static class DescriptionSize extends ValidationErrorPayload {
+        public DescriptionSize() {
+            super("DESCRIPTION_SIZE", "description size must be between 0 and 500");
+        }
+    }
+
+    public static class DueDateNull extends ValidationErrorPayload {
+        public DueDateNull() {
+            super("DUEDATE_NULL", "dueDate must not be null");
+        }
+    }
+
+    public static  class BaseTodoNull extends ValidationErrorPayload {
+        public BaseTodoNull() {
+            super("BASETODO_NULL", "baseTodo must not be null");
+        }
+    }
+}
