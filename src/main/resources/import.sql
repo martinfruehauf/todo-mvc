272afd8fd167c7e3d6b3cba38055ae6fa7443e99
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-08-17 13:36:32 +0200

    Add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,18 @@
+create table TAB_TODO (
+  COL_NAME text,
+  COL_DESC text,
+  COL_STATE boolean,
+  COL_DUE text
+);
+
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('Hydrocodone Bitartrate', 'congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede libero', true, '2019-10-09T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('Quetiapine fumarate', 'nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et', false, '2019-09-20T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('methylprednisolone acetate', 'in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt eu felis', true, '2019-07-07T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('Metronidazole', 'justo morbi ut odio cras mi pede malesuada in imperdiet et commodo vulputate justo in blandit', true, '2020-07-23T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('TRICLOSAN', 'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis', true, '2019-09-07T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('TITANIUM DIOXIDE', 'mi in porttitor pede justo eu massa donec dapibus duis', true, '2019-09-09T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('2 TITANIUM DIOXIDE', 'sdfede adsfasdfadf ', true, '2019-09-09T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('Broccoli', 'pellentesque volutpat dui maecenas tristique est et tempus semper est quam pharetra magna ac consequat metus sapien ut nunc vestibulum', true, '2020-03-09T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('ACONITUM NAPELLUS', 'sapien cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus', false, '2020-09-29T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('PRAVASTATIN SODIUM', 'nulla sed vel enim sit amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur', false, '2020-02-13T23:11:00');
+insert into TAB_TODO (COL_NAME, COL_DESC, COL_STATE, COL_DUE) values ('Austrian Pine', 'nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit', false, '2020-02-17T23:11:00');
